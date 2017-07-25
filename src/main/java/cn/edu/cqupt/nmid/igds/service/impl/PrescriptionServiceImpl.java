2edd0208966a462998571e78abfc1b6e57d2eba8
Kevin <1284629130@qq.com>
2017-07-25 19:53:34 +0800

    存取药方服务层实现

---
+++ src/main/java/cn/edu/cqupt/nmid/igds/service/impl/PrescriptionServiceImpl.java
@@ -0,0 +1,19 @@
+package cn.edu.cqupt.nmid.igds.service.impl;
+
+import cn.edu.cqupt.nmid.igds.model.json.ResponseJson;
+import cn.edu.cqupt.nmid.igds.service.PrescriptionService;
+
+/**
+ * Created by Administrator on 2017/7/25.
+ */
+public class PrescriptionServiceImpl implements PrescriptionService {
+    @Override
+    public ResponseJson savePrescription(int linkId, String PrescriptionMessage) {
+        return null;
+    }
+
+    @Override
+    public ResponseJson getPrescription(int linkId) {
+        return null;
+    }
+}
